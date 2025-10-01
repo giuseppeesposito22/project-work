34dae877da3e0a25f383e02885ddcbe69b96b400
giuseppeesposito22 <gius.espo22@gmail.com>
2025-10-01 20:04:31 +0200

    resolve problem

--- src/test/java/com/project/java/project_work/ProjectWorkApplicationTests.java
+++ src/test/java/com/project/java/project_work/ProjectWorkApplicationTests.java
@@ -1,8 +1,5 @@
 package com.project.java.project_work;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
 @SpringBootTest
 class ProjectWorkApplicationTests {
 
---
+++ src/test/java/com/project/java/project_work/SpringBootTest.java
@@ -0,0 +1,5 @@
+package com.project.java.project_work;
+
+public @interface SpringBootTest {
+
+}
