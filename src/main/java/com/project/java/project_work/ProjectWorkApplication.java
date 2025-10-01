c485ac8d3f6f13eb8447e41dc42537856485429d
giuseppeesposito22 <gius.espo22@gmail.com>
2025-10-01 18:37:35 +0200

    first commit

---
+++ src/main/java/com/project/java/project_work/ProjectWorkApplication.java
@@ -0,0 +1,13 @@
+package com.project.java.project_work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectWorkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/java/project_work/ProjectWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.java.project_work;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
