13fd370d8d21d76e72a51c6ba5c797161a9e3868
giuseppeesposito22 <gius.espo22@gmail.com>
2025-10-02 14:11:16 +0200

    add create in controller

--- src/main/java/com/project/java/project_work/controller/VideogameController.java
+++ src/main/java/com/project/java/project_work/controller/VideogameController.java
@@ -3,12 +3,18 @@ package com.project.java.project_work.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.project.java.project_work.model.Videogame;
+import com.project.java.project_work.service.PlatformService;
 import com.project.java.project_work.service.VideogameService;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/videogames")
@@ -17,7 +23,8 @@ public class VideogameController {
     @Autowired
     private VideogameService videogameService;
 
-
+    @Autowired
+    private PlatformService platformService;
 
 
     @GetMapping
@@ -40,7 +47,24 @@ public class VideogameController {
     public String create(Model model){
         
         model.addAttribute("videogame", new Videogame());
+        model.addAttribute("platforms", platformService.getAllPlatforms());
         
         return "videogames/create-update";    
+    }
+
+    @PostMapping("/create")
+    public String store(@Valid @ModelAttribute("videogame") Videogame videogame, 
+     BindingResult bindingResult, Model model 
+    ){
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("platforms", platformService.getAllPlatforms());
+            return "/videogames/create-update";
+        }
+
+        videogameService.createVideogame(videogame);
+
+
+        return "redirect:/videogames";
     }
 }
