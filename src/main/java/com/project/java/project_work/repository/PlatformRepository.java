5bcae13362dc6263b220529653d0a48ad480c091
giuseppeesposito22 <gius.espo22@gmail.com>
2025-10-02 13:09:05 +0200

    add platform repository

---
+++ src/main/java/com/project/java/project_work/repository/PlatformRepository.java
@@ -0,0 +1,9 @@
+package com.project.java.project_work.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.project.java.project_work.model.Platform;
+
+public interface PlatformRepository extends JpaRepository<Platform, Integer>{
+
+}
