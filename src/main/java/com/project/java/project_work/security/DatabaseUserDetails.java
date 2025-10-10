1445ddb43229ebaccddc9045f0bd6a62ca0da288
giuseppeesposito22 <gius.espo22@gmail.com>
2025-10-10 12:35:38 +0200

    add final to property in database user details

--- src/main/java/com/project/java/project_work/security/DatabaseUserDetails.java
+++ src/main/java/com/project/java/project_work/security/DatabaseUserDetails.java
@@ -12,10 +12,10 @@ import com.project.java.project_work.model.User;
 
 public class DatabaseUserDetails implements UserDetails{
 
-    private Integer id;
-    private String username;
-    private String password;
-    private Set<GrantedAuthority> authorities;
+    private final Integer id;
+    private final String username;
+    private final String password;
+    private final Set<GrantedAuthority> authorities;
 
 
 
@@ -63,28 +63,19 @@ public class DatabaseUserDetails implements UserDetails{
     public Integer getId() {
         return id;
     }
-    public void setId(Integer id) {
-        this.id = id;
-    }
+ 
     public String getUsername() {
         return username;
     }
-    public void setUsername(String username) {
-        this.username = username;
-    }
+   
     public String getPassword() {
         return password;
     }
-    public void setPassword(String password) {
-        this.password = password;
-    }
+   
     public Set<GrantedAuthority> getAuthorities() {
         return authorities;
     }
-    public void setAuthorities(Set<GrantedAuthority> authorities) {
-        this.authorities = authorities;
-    }
-
+   
 
 
     
